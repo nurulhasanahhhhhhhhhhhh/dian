3214373265606eee0768b286778a8bf9e8840f3b
nurulhasanahhhhhhhhhhhh <nrulhasanah224@gmail.com>
2024-01-22 12:04:05 +0700

    first commit

---
+++ 2pw.php
@@ -0,0 +1,10 @@
+<?
+$bill=100;
+$bil2-20;
+$teks1="PHP";
+$teks2="php";
+$hasil ($bill $bil2) or ($teks1=$teks2); 
+echo "$bill $bil2 or $teks 1- $teks2 adalah Shasil<br>";
+$hasil !($teks1=$teks2);
+echo"!($teks1=$teks2) adalah $hasil";
+?>
